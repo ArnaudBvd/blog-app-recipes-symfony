7b5b9b80fdc0c393357ef5471e76a848712e88f9
ArnaudBvd <bravard.arnaud@gmail.com>
2024-10-29 17:39:27 +0100

    feat: ajout d'une méthode pour hasher les mots de passe ainsi que création de deux utilisateurs tests dans UserFixtures.php

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,16 +2,38 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
+    private $hasher;
+        public function __construct(UserPasswordHasherInterface $hasher){
+            $this->hasher = $hasher;
+        }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $testUser = new User();
+        $testUser->setFirstname('user');
+        $testUser->setLastname('user');
+        $testUser->setEmail('user@user.fr');
+        $encodedPassword = $this->hasher->hashPassword($testUser, 'user');
+        $testUser->setPassword($encodedPassword);
+        $testUser->setRoles(['ROLE_USER']);
 
+        $testAdmin = new User();
+        $testAdmin->setFirstname('admin');
+        $testAdmin->setLastname('admin');
+        $testAdmin->setEmail('admin@admin.fr');
+        $encodedPassword = $this->hasher->hashPassword($testAdmin, 'admin');
+        $testAdmin->setPassword($encodedPassword);
+        $testAdmin->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($testUser);
+        $manager->persist($testAdmin);
         $manager->flush();
     }
 }
