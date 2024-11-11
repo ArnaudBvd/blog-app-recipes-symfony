03c8483c3759d6a8bf6b17eb3887e95479b42a2c
ArnaudBvd <bravard.arnaud@gmail.com>
2024-11-11 17:28:34 +0100

    feat: ajout des champs dans le formulaire Recipe

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -6,6 +6,10 @@ use App\Entity\Recipe;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,21 +18,60 @@ class RecipeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('cooking_time')
-            ->add('preparation_time')
-            ->add('serving')
-            ->add('difficulty')
-            ->add('category')
+            ->add('title', TextType::class, [
+                'label' => 'Nom de la recette :',
+                'attr' => [
+                    'placeholder' => 'Veuillez renseigner un nom de recette'
+                ]
+            ])
+            ->add('description', TextType::class, [
+                'label' => 'Description de la recette : ',
+                'attr' => [
+                    'placeholder' => 'Veuillez fournir une description de la recette'
+                ]
+            ])
+            ->add('cooking_time', IntegerType::class, [
+                'label' => 'Temps de cuisson (en minute) : ',
+                'attr' => [
+                    'min' => 1
+                ]
+            ])
+            ->add('preparation_time', IntegerType::class, [
+                'label' => 'Temps de préparation (en minute) :',
+                'attr' => [
+                    'min' => 1
+                ]
+            ])
+            ->add('serving', NumberType::class, [
+                'label' => 'Nombre de parts :',
+                'html5' => true,
+                'attr' => [
+                    'min' => 1,
+                    'max' => 20
+                ]
+            ])
+            ->add('difficulty', ChoiceType::class, [
+                'label' => 'Difficulté :',
+                'choices' => [
+                    'Facile' => 'facile',
+                    'Moyenne' => 'moyenne',
+                    'Élevée' => 'élevée'
+                ]
+            ])
+            ->add('category', ChoiceType::class, [
+                'label' => 'Catégorie :',
+                'choices' => [
+                    'Entrée' => 'entrée',
+                    'Plat' => 'plat',
+                    'Dessert' => 'dessert'
+                ]
+            ])
             ->add('created_at', null, [
+                'label' => 'Date de création :',
                 'widget' => 'single_text',
             ])
             ->add('picture')
-            ->add('user', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => 'id',
-            ])
+
         ;
     }
 
